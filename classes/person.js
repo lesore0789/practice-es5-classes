1c1818de7b1cd3aa5e4cdf564e2245553849afae
Leslie <lesore0789@gmail.com>
2023-02-14 00:28:57 -0800

    Refactor Person class to ES5

--- classes/person.js
+++ classes/person.js
@@ -1,24 +1,43 @@
 const Dog = require('./dog');
 
-class Person {
-  constructor(name, age, dogs = []) {
-    this.name = name;
-    this.age = age;
-    this.dogs = dogs;
-  }
+// class Person {
+//   constructor(name, age, dogs = []) {
+//     this.name = name;
+//     this.age = age;
+//     this.dogs = dogs;
+//   }
 
-  addDog(dog) {
-    if (!(dog instanceof Dog)) {
-      throw new TypeError("Can only add dogs!");
-    }
-    this.dogs.push(dog);
-  }
+//   addDog(dog) {
+//     if (!(dog instanceof Dog)) {
+//       throw new TypeError("Can only add dogs!");
+//     }
+//     this.dogs.push(dog);
+//   }
 
-  walkDogs() {
-    this.dogs.forEach((dog) => {
-      console.log(`walking ${dog.name}`);
-    });
+//   walkDogs() {
+//     this.dogs.forEach((dog) => {
+//       console.log(`walking ${dog.name}`);
+//     });
+//   }
+// }
+
+function Person(name, age, dogs = []){
+  this.name = name;
+  this.age = age;
+  this.dogs = dogs;
+}
+
+Person.prototype.addDog = function(dog){
+  if(!(dog instanceof Dog)) {
+    throw new TypeError('Can only add dogs!')
   }
+  this.dogs.push(dog)
+}
+
+Person.prototype.walkDogs = function(){
+  this.dogs.forEach(function(dog){
+    console.log(`walking ${dog.name}`)
+  })
 }
 
 /****************************************************************************/
